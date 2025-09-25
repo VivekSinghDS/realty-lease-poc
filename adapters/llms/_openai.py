12674693bb7360d07cef2c879cb36fd16385fbd8
Vivek Singh <viveknagesh0301@gmail.com>
2025-09-25 17:26:53 +0530

    added the correction in amendments

--- adapters/llms/_openai.py
+++ adapters/llms/_openai.py
@@ -10,10 +10,18 @@ class _OpenAI(LargeLanguageModel):
         self.model_name = model_name
         
     def get_streaming_response(self, payload: List[dict]):
-        pass 
+        print('inside this method')
+        return self.client.responses.create(
+            model="gpt-4o",
+            input=payload,
+            stream=True,
+            temperature = 0
+        ) 
     
     def get_non_streaming_response(self, payload: List[dict]):
         return self.client.responses.create(
             model=self.model_name,
-            input=payload
+            input=payload,
+            max_output_tokens=19999,
+        
         ) 
